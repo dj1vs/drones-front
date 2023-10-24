b9ea09bcaa714006f2b939a9d4872c1e399c16c0
Djivs <icemcpe099@gmail.com>
2023-10-24 20:05:32 +0300

    start option assignment

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import ITunesPage from './ITunesPage'
+// import './index.css'
+// import ITunesPage from './ITunesPage'
+import RegionsPage from './RegionsPage'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ITunesPage />
+    <RegionsPage />
   </React.StrictMode>,
 )

7a580660f90d149cabbe5ba0483ab278d3e6f26f
dj1vs <datrifonov03@gmail.com>
2023-10-26 19:17:31 +0300

    region page finished

--- src/RegionPage.tsx
+++ src/RegionPage.tsx
@@ -1,21 +1,43 @@
-import {FC, useState} from 'react'
+import {FC, useEffect, useState} from 'react'
 import './RegionPage.css'
 import {Region, getRegionByName } from './modules/get-region-by-name'
 
 const RegionPage: FC = () => {
 
     const [region, setRegion] = useState<Region>()
-    
-    const test = async () => {
-        const result = await getRegionByName('Кузьминки')
-        setRegion(result)
-    }
 
-    test()
+    useEffect(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString)
+        const regionName = urlParams.get('region_name')
+    
+        const getRegion = async () => {
+            const result = await getRegionByName(String(regionName))
+            console.log(result)
+            setRegion(result)
+        }
+    
+        getRegion()
+
+    }, []);
     
     return (
         <div>
-            <h2> Округ: {region?.Name} </h2>
+            <img src={"data:image/jpg;base64, " + region?.Image} className="card_image" />
+            <p>{region?.Details}</p>
+            <p className="region_line"><b>Статус района: {region?.Status}</b></p>
+            <p className="region_line"> Площадь: {region?.AreaKm} км^2</p>
+            <p className="region_line"> Население: {region?.Population} чел.</p>
+            <p className="region_line"> Глава управы: {region?.HeadName}</p>
+            <p className="region_line"> Email главы управы: {region?.HeadEmail}</p>
+            <p className="region_line"> Телефон главы управы: {region?.HeadPhone}</p>
+            <p className="region_line"> Средняя высота: {region?.AverageHeightM}</p>
+            <p className="region_line">Описание района: {region?.Details}</p>
+            <form method="POST" action="delete_region/{{ .Name }}" name="delete_region">
+                <input type="hidden" name="card_title" id="card_title" value="{{ .Name }}" />
+                <input type="submit" className="button page_button" value="Сменить статус" />
+            </form>
+            <a className="button page_button" href="..">Домой</a>
         </div>
     )
 }
--- src/RegionsPage.tsx
+++ src/RegionsPage.tsx
@@ -1,10 +1,18 @@
-import {FC} from 'react'
+import {FC, useEffect} from 'react'
 import './RegionsPage.css'
 
 const RegionsPage: FC = () => {
+    useEffect(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString)
+        const regionName = urlParams.get('region_name')
+
+        console.log(regionName)
+    }, []);
+
     return (
         <div>
-            <form method="GET" action="region" name="search">
+            <form method="GET" action="region_search" name="search">
             <label htmlFor="region_name">Введите название:</label>
             <input type="text" id="region_name" name="region_name"/>
             <input type="submit" className="button" value="Поиск" ></input>

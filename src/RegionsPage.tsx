23bb55b99372963678e089e81093e9240a68ba0c
Djivs <icemcpe099@gmail.com>
2023-10-27 18:52:34 +0300

    update

--- src/RegionsPage.tsx
+++ src/RegionsPage.tsx
@@ -5,26 +5,41 @@ import { Region } from './modules/ds'
 import { getRegions } from './modules/get-regions';
 
 import { Card, Col, Row} from 'react-bootstrap'
+import RegionCard from './components/RegionCard';
 
 
 const RegionsPage: FC = () => {
 
-    const [regions, setRegions] = useState<Region[]>()
+    const [regions, setRegions] = useState<Region[]>([])
 
     useEffect(() => {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString)
-        const regionName = urlParams.get('region_name')
+        var regionName = urlParams.get('region_name')
+        if (regionName == null) {
+            regionName = "";
+        }
 
         const loadRegions = async()  => {
             const result = await getRegions(String(regionName))
-            console.log(result)
             setRegions(result)
+
         }
 
         loadRegions()
 
     }, []);
+
+    let regionCards = []
+
+    let i = 0
+    while (i < regions.length) {
+        regionCards.push(RegionCard({
+            imageUrl: regions[i].Image,
+            regionName: regions[i].Name,
+            pageUrl: window.location.href + "/region?region_name=" + regions[i].Name
+        }));
+    }
 
     return (
         <div>
@@ -35,6 +50,8 @@ const RegionsPage: FC = () => {
                 <input type="submit" className="button" value="Поиск" ></input>
                 </form>
             </div>
+            
+
         </div>
         
     )
---
+++ src/components/RegionCard.tsx
@@ -0,0 +1,23 @@
+import {FC} from 'react'
+import {Button, Card} from 'react-bootstrap'
+import './RegionCard.css'
+
+interface Props {
+    imageUrl: string
+    regionName: string
+    pageUrl: string
+}
+
+const RegionCard: FC<Props> = ({ imageUrl, regionName, pageUrl }) => (
+    <Card className='card'>
+        <Card.Img className="cardImage" variant="top" src={"data:image/jpg;base64, " + {imageUrl}} height = {100} width = {100} />
+        <Card.Body>
+            <div className='textStyle'>
+                <Card.Title> {regionName} </Card.Title>
+            </div>
+        <Button className='cardButton' href={pageUrl}> Подробнее </Button>
+        </Card.Body>
+    </Card>
+)
+
+export default RegionCard;

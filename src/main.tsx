68b8286063917d38232c08e7d2f09a14385f496d
dj1vs <datrifonov03@gmail.com>
2023-11-17 23:08:12 +0300

    try browswer router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,22 +10,15 @@ import RegionPage from './RegionPage'
 import Navigation from './components/Navigation'
 import Breadcrumbs from './components/Breadcrumbs';
 
-const router = createBrowserRouter([
-  {
-    path: '/drones-front',
-    element: <RegionsPage />
-  },
-  {
-    path: '/drones-front/region',
-    element: <RegionPage />
-  }
-])
-
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Navigation />
-    <Breadcrumbs />
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Navigation />
+      <Breadcrumbs />
+      <Routes>
+        <Route path="/drones-front" Component={RegionsPage} />
+        <Route path="/drones-front/region" Component={RegionPage} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

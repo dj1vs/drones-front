eba321c411a3fb2eb7e17374d7546343343fafd2
Djivs <icemcpe099@gmail.com>
2024-01-17 20:05:30 +0300

    fix offline search

--- src/modules/get-regions.tsx
+++ src/modules/get-regions.tsx
@@ -14,54 +14,65 @@ export const getRegions = async (userToken = '', namePattern = '', status='', di
         },
     }
 
+    const offline_regions: Region[] =  [
+        {
+            ID: 1,
+            District: "",
+            Name: "Район #1",
+            Details: "",
+            Status: "Действует",
+            AreaKm: '0',
+            Population: 0,
+            HeadName: "",
+            HeadEmail: "",
+            HeadPhone: "",
+            AverageHeightM: 0,
+            ImageName: ""
+        },
+        {
+            ID: 2,
+            District: "",
+            Name: "Район #2",
+            Details: "",
+            Status: "Действует",
+            AreaKm: '0',
+            Population: 0,
+            HeadName: "",
+            HeadEmail: "",
+            HeadPhone: "",
+            AverageHeightM: 0,
+            ImageName: ""
+        },
+        {
+            ID: 3,
+            District: "",
+            Name: "Район #3",
+            Details: "",
+            Status: "Действует",
+            AreaKm: '0',
+            Population: 0,
+            HeadName: "",
+            HeadEmail: "",
+            HeadPhone: "",
+            AverageHeightM: 0,
+            ImageName: ""
+        },];
+
     return axios.get(
         '/api/regions?name_pattern=' + String(namePattern) + '&status=' + String(status) + '&district=' + String(district),
         config
         )
         .then((response) => response.data)
-        .catch(() => (
-                { "regions": [
-                    {
-                        ID: 1,
-                        District: "",
-                        Name: "Район #1",
-                        Details: "",
-                        Status: "Действует",
-                        AreaKm: 0,
-                        Population: 0,
-                        HeadName: "",
-                        HeadEmail: "",
-                        HeadPhone: "",
-                        AverageHeightM: 0,
-                        ImageName: ""
-                    },
-                    {
-                        ID: 2,
-                        District: "",
-                        Name: "Район #2",
-                        Details: "",
-                        Status: "Действует",
-                        AreaKm: 0,
-                        Population: 0,
-                        HeadName: "",
-                        HeadEmail: "",
-                        HeadPhone: "",
-                        AverageHeightM: 0,
-                        ImageName: ""
-                    },
-                    {
-                        ID: 3,
-                        District: "",
-                        Name: "Район #3",
-                        Details: "",
-                        Status: "Действует",
-                        AreaKm: 0,
-                        Population: 0,
-                        HeadName: "",
-                        HeadEmail: "",
-                        HeadPhone: "",
-                        AverageHeightM: 0,
-                        ImageName: ""
-                    },]
-                }))
+        .catch(() => {
+            let result_regions : Region[] = []
+            for (const region of offline_regions) {
+                if (region.Name.toLowerCase().indexOf(namePattern.toLowerCase()) !== -1) {
+                    result_regions.push(region);
+                }
+            }
+
+            return {
+                "regions" : result_regions
+            }
+        })
 }
